f47f75525ba3fcf9bad4d864d4b5fb8c4b34c080
Zoltán Katona <36472477+ka-zo@users.noreply.github.com>
2025-07-26 16:31:59 +0200

    Flutter package versions are upgraded, single function to search and replace and append keys and values in files is added.

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -8,41 +8,57 @@ import 'licenses.dart';
 import 'utils.dart' as utils;
 
 Future<void> run(HookContext context) async {
+  const String localPropertiesFile = './android/local.properties';
+  const String buildGradleKts = './android/app/build.gradle.kts';
+
   List<String> flutterPackages = [
-    'flutter_localization:^0.2.0',
-    'flutter_native_splash:^2.3.10',
-    'logger:^2.0.2+1',
-    'package_info_plus:^5.0.1',
-    'shared_preferences:^2.2.2',
+    'flutter_localization:^0.3.3',
+    'flutter_native_splash:^2.4.6',
+    'logger:^2.6.0',
+    'package_info_plus:^8.3.0',
+    'shared_preferences:^2.5.3',
   ];
 
   if (context.vars['use_authentication']) {
     flutterPackages += [
-      'firebase_core:^2.25.4',
-      'firebase_auth: ^4.17.4',
-      'firebase_ui_auth:^1.13.0',
+      'firebase_core:^3.15.1',
+      'firebase_auth: ^5.6.2',
+      'firebase_ui_auth:^1.17.0',
       'google_sign_in:^6.2.1',
-      'firebase_ui_oauth_google:^1.3.0',
-      'firebase_ui_localizations:^1.10.2',
+      'firebase_ui_oauth_google:^1.4.2',
+      'firebase_ui_localizations:^1.14.0',
     ];
     final Map fontsSocialIcons = {
-        'family': 'SocialIcons',
-        'fonts': [{
-            'asset': 'packages/firebase_ui_auth/fonts/SocialIcons.ttf'
-        }]
+      'family': 'SocialIcons',
+      'fonts': [
+        {'asset': 'packages/firebase_ui_auth/fonts/SocialIcons.ttf'}
+      ]
     };
     _addFontsToPubspec(context: context, fonts: fontsSocialIcons);
   }
 
   final Set<String> assets = {
-      'android/app/src/main/res/mipmap-xxhdpi/',
-      'assets/',
+    'android/app/src/main/res/mipmap-xxhdpi/',
+    'assets/',
   };
   _addAssetsToPubspec(context: context, assets: assets);
 
   context.vars['license'] = licenses[context.vars['license']];
 
-  _setFlutterMinSdkLevel(context: context, minSdkVersion: 21);
+  _setKeyInFile(
+      context: context,
+      filePath: localPropertiesFile,
+      key: "minSdkVersion",
+      value: 23,
+      appendIfMissing: true);
+  _setKeyInFile(
+      context: context, filePath: buildGradleKts, key: "minSdk", value: 23);
+
+  _setKeyInFile(
+      context: context,
+      filePath: buildGradleKts,
+      key: "ndkVersion",
+      value: "27.0.12077973");
 
   final progress = context.logger.progress("Installing packages...");
   for (String e in flutterPackages) {
@@ -61,70 +77,99 @@ Future<void> run(HookContext context) async {
   progress.complete();
 }
 
-void _addAssetsToPubspec({
-  required HookContext context,
-  required Set<String> assets
-}) {
+void _addAssetsToPubspec(
+    {required HookContext context, required Set<String> assets}) {
   final String content = File('pubspec.yaml').readAsStringSync();
   final pubspec = YamlEditor(content);
 
   try {
-      final YamlList assetsNode = pubspec.parseAt(['flutter', 'assets']) as YamlList;
-      final assetsList = assetsNode.toSet().union(assets).toList();
-      pubspec.update(['flutter', 'assets'], assetsList);
-      context.logger.info("Assets are added to pubspec.yaml.");
+    final YamlList assetsNode =
+        pubspec.parseAt(['flutter', 'assets']) as YamlList;
+    final assetsList = assetsNode.toSet().union(assets).toList();
+    pubspec.update(['flutter', 'assets'], assetsList);
+    context.logger.info("Assets are added to pubspec.yaml.");
   } on ArgumentError catch (e) {
 //      context.logger.info(e);
-      context.logger.info('Creating flutter / assets path. It does not yet exist.');
-      pubspec.update(['flutter', 'assets'], assets.toList());
+    context.logger
+        .info('Creating flutter / assets path. It does not yet exist.');
+    pubspec.update(['flutter', 'assets'], assets.toList());
   }
 
   File('pubspec.yaml').writeAsStringSync(pubspec.toString());
 }
 
-void _addFontsToPubspec({
-  required HookContext context,
-  required Map fonts
-}) {
+void _addFontsToPubspec({required HookContext context, required Map fonts}) {
   final String content = File('pubspec.yaml').readAsStringSync();
   final pubspec = YamlEditor(content);
 
   try {
-      final YamlList fontsNode = pubspec.parseAt(['flutter', 'fonts']) as YamlList;
-      bool hasFonts = false;
-      fontsNode.forEach(
-          (item) {
-              if (item['family'] == fonts['family']) {
-                  hasFonts = true;
-              }
-          }
-      );
-      if (!hasFonts) {
-          pubspec.prependToList(['flutter', 'fonts'], fonts);
-          context.logger.info("Font ${fonts['family']} is added to pubspec.yaml.");
-      } else {
-          context.logger.info("Font ${fonts['family']} is already in pubspec.yaml.");
+    final YamlList fontsNode =
+        pubspec.parseAt(['flutter', 'fonts']) as YamlList;
+    bool hasFonts = false;
+    fontsNode.forEach((item) {
+      if (item['family'] == fonts['family']) {
+        hasFonts = true;
       }
+    });
+    if (!hasFonts) {
+      pubspec.prependToList(['flutter', 'fonts'], fonts);
+      context.logger.info("Font ${fonts['family']} is added to pubspec.yaml.");
+    } else {
+      context.logger
+          .info("Font ${fonts['family']} is already in pubspec.yaml.");
+    }
   } on ArgumentError catch (e) {
 //      context.logger.info(e);
-      context.logger.info('Creating flutter / fonts path. It does not yet exist.');
-      context.logger.info("Font ${fonts['family']} is added to pubspec.yaml.");
-      pubspec.update(['flutter', 'fonts'], [fonts]);
+    context.logger
+        .info('Creating flutter / fonts path. It does not yet exist.');
+    context.logger.info("Font ${fonts['family']} is added to pubspec.yaml.");
+    pubspec.update(['flutter', 'fonts'], [fonts]);
   }
 
   File('pubspec.yaml').writeAsStringSync(pubspec.toString());
 }
 
-void _setFlutterMinSdkLevel({required HookContext context, required int minSdkVersion}) {
-  const String localPropertiesFile = './android/local.properties';
+void _setKeyInFile({
+  required HookContext context,
+  required String filePath,
+  required String key,
+  required Object value,
+  bool appendIfMissing = false,
+}) {
+  final formattedValue = value is String ? '"$value"' : value;
 
-  String content = File(localPropertiesFile).readAsStringSync();
-  final String newProperty = "flutter.minSdkVersion=$minSdkVersion";
-  context.logger.info("Setting minSdkVersion to $minSdkVersion in $localPropertiesFile");
-  if (content.contains('flutter.minSdkVersion')) {
-    content = content.replaceFirst(RegExp('^flutter.minSdkVersion(.*)', multiLine: true, dotAll: true), newProperty);
-    File(localPropertiesFile).writeAsStringSync(content);
-  } else {
-    File(localPropertiesFile).writeAsStringSync("\n$newProperty", mode:FileMode.append);
+  try {
+    if (!File(filePath).existsSync()) {
+      print('Error: $filePath does not exist.');
+      return;
+    }
+
+    String content = File(filePath).readAsStringSync();
+
+    final RegExp regex = RegExp('^([ \\t]*$key\\s*=\\s*).*', multiLine: true);
+
+    // Replace all occurrences where the key is found.
+    // The replacement string combines the captured prefix (Group 1, e.g., "  setting_name=")
+    // with the new value.
+    final String modifiedContent = content.replaceAllMapped(regex, (match) {
+      // match.group(1) contains the prefix (leading whitespace + key=).
+      // We use '!' because if a match occurs, group(1) will always be non-null.
+      return '${match.group(1)!}$formattedValue';
+    });
+
+    if (regex.hasMatch(content)) {
+      File(filePath).writeAsStringSync(modifiedContent);
+      context.logger.info('$key updated to $value in $filePath');
+    } else if (appendIfMissing) {
+      File(filePath).writeAsStringSync(
+        content + '\n$key=$formattedValue',
+      );
+      context.logger
+          .info('$key appended as $key = $formattedValue in $filePath');
+    } else {
+      context.logger.info('$key not found. No update performed in $filePath.');
+    }
+  } catch (e) {
+    context.logger.info('Error updating $key in $filePath: $e');
   }
 }
